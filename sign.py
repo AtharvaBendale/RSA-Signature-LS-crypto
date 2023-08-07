55fbe987fbf6da8610e297e16e193693491877c8
AtharvaBendale <113408969+AtharvaBendale@users.noreply.github.com>
2023-08-08 03:36:14 +1400

    Add files via upload

--- sign.py
+++ sign.py
@@ -60,12 +60,123 @@ def generate_signature():
          "0x19a4c116", "0x1e376c08", "0x2748774c", "0x34b0bcb5", "0x391c0cb3", "0x4ed8aa4a", "0x5b9cca4f", "0x682e6ff3",
          "0x748f82ee", "0x78a5636f", "0x84c87814", "0x8cc70208", "0x90befffa", "0xa4506ceb", "0xbef9a3f7", "0xc67178f2"]
 
-    # Rest of the code...
+filen=input("Enter your filename(without .txt) : ")
+text_file=open("{0}.txt".format(filen),"r")
+sign=text_file.read()
 
-def verify_signature():
-    # Implement verification logic...
-    pass
+sign_bits=str_to_bits(sign)
+# print(sign_bits)
+len_bits=len(sign_bits)
 
-if __name__ == "__main__":
-    # Modify the main code as per your requirements...
-    pass
+append_size=len_bits%512
+if(append_size>448):
+    append_size=960-append_size
+elif(append_size<=448):
+    append_size=448-append_size
+if(append_size>=1):
+    sign_bits=sign_bits+"1"
+    append_size-=1
+    for i in range(append_size):
+        sign_bits=sign_bits+"0"
+# print(sign_bits)
+# print(len(sign_bits))
+
+for i in range(64-len((bin(len_bits))[2:])):
+    sign_bits=sign_bits+"0"
+# print(bin_len)    
+# print(type(bin_len))
+sign_bits=sign_bits+str(bin(len_bits))[2:]
+# print(sign_bits)    
+# print(len(sign_bits))
+findig=""
+for i in range(len(sign_bits)//512):
+
+    M=[]
+    ccd=0
+    for i in range(16):
+        M.append(sign_bits[ccd:ccd+32])
+        ccd+=32
+    dump=""
+    for i in range(32):
+        dump=dump+"0"
+    # print("hi")
+    # print(len(dump))
+    for i in range(48):
+        M.append(dump)
+    
+    # w=[]
+    # for i in range(64):
+    #     w.append(M[i])
+    for i in range(16,64):
+        
+        v1=right_rotate_string(M[i-15],7)
+        v2=right_rotate_string(M[i-15],18)
+        v3=right_shift_string(M[i-15],3)
+        s0=xorstring(v1,v2,v3)
+        
+        v4=right_rotate_string(M[i-2],17)
+        v5=right_rotate_string(M[i-2],19)
+        v6=right_shift_string(M[i-2],10)
+        s1=xorstring(v4,v5,v6)
+        M[i]=binsum(M[i-16],s0,M[i-7],s1)[-32:]
+    # print(M)
+
+    for i in range(64):
+        s1=xorstring(right_rotate_string(e,6),right_rotate_string(e,11),right_rotate_string(e,25))
+        ch= int(bitand(e,f),2) ^ int(bitand(bitnot(e),g),2)
+        ch='{0:b}'.format(ch)
+        temp1=binsum(h,s1,ch,M[i])
+        kdump=str( "{0:08b}".format(int(k[i], 16)))
+        temp1=str(bin(int(temp1,2) + int(kdump,2))[2:])[-32:]
+        s0=xorstring(right_rotate_string(a,2),right_rotate_string(a,13),right_rotate_string(a,22))
+        maj=xorstring(bitand(a,b),bitand(a,c),bitand(b,c))
+        temp2=bin(int(s0,2) + int(maj,2))
+        temp2=str(temp2[2:])[-32:]
+        h=g
+        g=f
+        f=e
+        e=bin(int(d,2) + int(temp1,2)) 
+        e=str(e[2:])[-32:]
+        d=c
+        c=b
+        b=a
+        a=bin(int(temp1,2) + int(temp2,2))
+        a=str(a[2:])[-32:]
+    H_0=bin(int(H_0,2) + int(a,2)) 
+    H_0=str(H_0[2:])[-32:]
+    H_1=bin(int(H_1,2) + int(b,2)) 
+    H_1=str(H_1[2:])[-32:]
+    H_2=bin(int(H_2,2) + int(c,2)) 
+    H_2=str(H_2[2:])[-32:]
+    H_3=bin(int(H_3,2) + int(d,2)) 
+    H_3=str(H_3[2:])[-32:]
+    H_4=bin(int(H_4,2) + int(e,2)) 
+    H_4=str(H_4[2:])[-32:]
+    H_5=bin(int(H_5,2) + int(f,2)) 
+    H_5=str(H_5[2:])[-32:]
+    H_6=bin(int(H_6,2) + int(g,2)) 
+    H_6=str(H_6[2:])[-32:]
+    H_7=bin(int(H_7,2) + int(h,2)) 
+    H_7=str(H_7[2:])[-32:]
+digest= H_0 +H_1 +H_2+H_3+H_4+H_5+H_6+H_7
+    # digest=int(digest,2)
+    # digest=hex(digest)[2:]
+findig=findig+digest
+# if(len(sign_bits)==1024):
+# print(findig)
+findig=int(findig,2)
+# print(findig)
+
+#creating semi prime
+p=random_prime(2**200,True,2**180)
+q=random_prime(2**200,True,2**180)
+N=p*q
+phi=(p-1)*(q-1)
+e=65537
+dpri=inverse_mod(e,phi)
+S=pow(findig,int(dpri),int(N))
+S=hex(S)[2:]
+pubk=(N,e)
+# print("N : ",N)
+print("signature : ",S)
+print("public key : ",pubk)
